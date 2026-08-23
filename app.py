6ba1862031b05d53bb8da554c8932f1fd1738a3f
Gurnoor Singh <goat63262@gmail.com>
2026-08-23 19:22:56 +0530

    remove test scaffold

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-import subprocess
-
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route("/ping")
-def ping():
-    host = request.args.get("host")
-    return subprocess.check_output("ping -c 1 " + host, shell=True)
-
-
-@app.route("/calc")
-def calc():
-    expr = request.args.get("expr")
-    return str(eval(expr))
+# test scaffold removed
